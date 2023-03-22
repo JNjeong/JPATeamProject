edebb45c6e3433f3fcc90aeed2cafb16624be0af
JNjeong <wnsghd0902@gmail.com>
2023-03-22 21:11:57 +0900

    project setup

---
+++ JPA_Gallery_Project/src/main/java/com/lec/spring/JpaGalleryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaGalleryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaGalleryProjectApplication.class, args);
+	}
+
+}
